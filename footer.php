046db44f7cc1d495c9c974db40bc4336c78ffd10
Garrett HackintoshOSX <garrettsurman@garrettsurman.com>
2018-11-19 18:24:14 -0500

    First commit

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php 
+echo"
+
+
+<div id='dl'>
+<a href='./downLoads/hw5.zip' download='Virus'>Download a Virus for free!</a>
+<p>&copy; 2016-" . date("Y") . " Garrett Surman</p>
+</div>";
+
+
+?>
---
+++ mobile/indexS.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+
+<!--[if lte IE 6]><html class="preIE7 preIE8 preIE9"><![endif]-->
+<!--[if IE 7]><html class="preIE8 preIE9"><![endif]-->
+<!--[if IE 8]><html class="preIE9"><![endif]-->
+<!--[if gte IE 9]><!-->
+<html>
+<!--<![endif]-->
+
+<head>
+    <meta charset="UTF-8">
+    <title>HW5 HW5 HW5 HW5 HW5 HW5</title>
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=font1|font2|etc" type="text/css">
+    <link rel="stylesheet" href="../css/styleS.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Bubbler+One" rel="stylesheet">
+
+</head>
+
+<body>
+    <h1>
+        It’s going to be a rainbow and unicorns kind of day.
+    </h1>
+
+
+
+
+    <!-- Container element -->
+    <div class="contentBG">
+        <div id="contentFG">
+            <h1 class="common">Facts</h1>
+
+            <ul id="list">
+                <div class="break"></div>
+                <li>Unicorns live in groups of four or five that make up a joyous family. The head of the family is an elder unicorn who is hundreds of years old. Other family members include a loving couple and one or two young unicorns.
+                    Unicorn families spend their lives alone living in peace deep in the forest.</li>
+
+                <div class="break"></div>
+                <li>When two unicorn families meet, they express great joy and travel together for weeks before they part and go their separate ways.</li>
+
+                <div class="break"></div>
+                <li> The unicorn may eat grass and hay, but it really doesn’t need to eat at all. It absorbs its energy from the sun, probably through the horn.</li>
+
+                <div class="break"></div>
+                <li>Unicorns can be any color, from jet-black and brown to dazzling gold, brilliant red or pure white.
+                    Anyone who touches a pure white unicorn will find happiness and joy for his entire life.
+                    The unicorn’s eyes are sky blue or purple.</li>
+
+                <div class="break"></div>
+                <li>Unicorns are earthbound and do not have wings. If a unicorn and a Pegasus (a flying horse) mate, the babies may become flying unicorns.</li>
+
+            </ul>
+
+            <div id="browser">
+                you are viewing this page from
+                <br>
+
+                <?php
+                        $useragent=$_SERVER['HTTP_USER_AGENT'];
+
+                        print $useragent;
+
+                        ?>
+
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
